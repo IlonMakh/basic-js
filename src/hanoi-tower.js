28983010e883e164342d40a518095117ad517cde
Ilona <mitsunaki@yandex.ru>
2022-04-30 17:14:41 +0300

    progress 11/21

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  let turn = 2**disksNumber - 1,
+      second = Math.floor((3600*turn)/turnsSpeed);
+
+  return {turns: turn, seconds: second};
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function getSumOfDigits(n) {
+  let sum = n + '';
+  while (sum.length !== 1) {
+    sum = sum.split("").map(Number);
+    sum = sum.reduce((total, elem) => total += elem, 0);
+    sum += "";
+  }
+  return +sum;
 }
 
 module.exports = {
