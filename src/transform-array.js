5205c87fc28c6ac0d6f9f24bc309a4c6f837b20f
Ilona <mitsunaki@yandex.ru>
2022-04-30 23:14:00 +0300

    progress 13/21, but with some fail in chain and array transform tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr) || !arr || !(arr instanceof Array)){
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
+  else {
+  let newArr = arr.slice(0);
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] == '--double-next') {
+      if (i == newArr.length - 1) {
+        newArr.splice(i, 1);
+      }
+      else newArr[i] = newArr[i+1];
+    } 
+    else if (newArr[i] == '--discard-prev') {
+      if (i == 0 || newArr[i-2] == '--discard-next') {
+        newArr.splice(i, 1);
+      }
+      else newArr.splice(i-1, 2);
+    }
+    else if (newArr[i] == '--discard-next') {
+      if (i == newArr.length - 1) {
+        newArr.splice(i, 1);
+      }
+      else newArr.splice(i, 2);
+    }
+    else if (newArr[i] == '--double-prev') {
+      if (i == 0 || newArr[i-2] == '--discard-next') {
+        newArr.splice(i, 1);
+      }
+      else newArr.splice(i-1, 1, newArr[i-1])
+    }
+  }
+  return newArr;
+}
+
 }
 
 module.exports = {
