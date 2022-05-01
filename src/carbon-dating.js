bbfa70605d92504d5e5c776f9699d94ed7093e51
Ilona <mitsunaki@yandex.ru>
2022-05-01 11:37:00 +0300

    progress 16/21, but with some fail tests in chain task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!Number(sampleActivity) || sampleActivity <= 0 || typeof sampleActivity !== 'string' || sampleActivity.length === 0 || sampleActivity > MODERN_ACTIVITY) return false;
+  let k = 0.693;
+  let time = (Math.log(MODERN_ACTIVITY/+sampleActivity)*HALF_LIFE_PERIOD)/k;
+  return Math.ceil(time);
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -18,37 +18,39 @@ function transform(arr) {
     throw new Error('\'arr\' parameter must be an instance of the Array!');
   }
 
-  else {
-  let newArr = arr.slice(0);
-  for (let i = 0; i < newArr.length; i++) {
-    if (newArr[i] == '--double-next') {
-      if (i == newArr.length - 1) {
-        newArr.splice(i, 1);
+  let newArr = arr.slice();
+
+for (let i=0; i<newArr.length; i++) {
+  if (newArr[i] === '--double-next') {
+      if(i === newArr.length-1) {
+         newArr.splice(i,1);   
+      } else {
+        newArr.splice(i,1,newArr[i+1]);
       }
-      else newArr[i] = newArr[i+1];
-    } 
-    else if (newArr[i] == '--discard-prev') {
-      if (i == 0 || newArr[i-2] == '--discard-next') {
-        newArr.splice(i, 1);
+  }
+  else if (newArr[i] === '--discard-next') {
+      if (newArr[i+2] == '--double-prev' || newArr[i+2] == '--discard-prev' ) {
+        newArr.splice(i,3);
+      } else {
+        newArr.splice(i,2);
       }
-      else newArr.splice(i-1, 2);
-    }
-    else if (newArr[i] == '--discard-next') {
-      if (i == newArr.length - 1) {
-        newArr.splice(i, 1);
-      }
-      else newArr.splice(i, 2);
-    }
-    else if (newArr[i] == '--double-prev') {
-      if (i == 0 || newArr[i-2] == '--discard-next') {
-        newArr.splice(i, 1);
-      }
-      else newArr.splice(i-1, 1, newArr[i-1])
+  }
+  else if (newArr[i] === '--double-prev') {
+    if(i === 0) {
+      newArr.splice(i,1);
+    } else {
+        newArr.splice(i,1,arr[i-1]);
     }
   }
-  return newArr;
-}
-
+  else if (newArr[i] === '--discard-prev') {
+    if(i === 0) {
+      newArr.splice(i,1);
+    } else {
+        newArr.splice(i-1,2);
+    }
+  }
+} 
+return newArr;
 }
 
 module.exports = {
